0f79ba8df546b84b4f198260ed7d98333988efe0
kevin <kcao36@outlook.com>
2025-09-14 22:14:38 -0600

    start new app

---
+++ app/page.tsx
@@ -0,0 +1,75 @@
+export default function Home() {
+  return (
+    <div style={styles.body}>
+      <div style={styles.header}>
+        <img src="/STARRLogoWhite.png" alt="STARR Logo" style={styles.logo} />
+        <div style={styles.title}>UASTARR<br/> MANAGEMENT</div>
+      </div>
+
+      <div style={styles.inputBoxes}>
+        <input 
+          type="text"
+          name="name"
+          placeholder="username"
+          style={styles.inputs}/>
+        <input 
+          type="password"
+          name="password"
+          placeholder="password"
+          style={styles.inputs}/>
+      </div>
+
+
+    </div>
+  );
+}
+
+const styles = {
+  body: {
+    backgroundColor: '#343434',
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column' as const,
+    alignItems: 'center'
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row' as const,
+    height: '20vh',
+    alignItems: 'stretch',
+  },
+  title: {
+    color: 'white',
+    fontFamily: 'monospace',
+    fontWeight: 'bold',
+    fontSize: '6vh',
+    display: 'flex',
+    flexDirection: 'column' as const,
+    justifyContent: 'center',
+    height: '100%',
+  },
+  logo: {
+    width: "auto",
+    height: "100%",
+    margin: 10
+  },
+  inputBoxes: {
+    margin: 300,
+    display: 'flex',
+    flexDirection: 'column' as const
+  },
+  inputs: {
+    width: '300px',
+    height: '40px',
+    padding: '10px',
+    fontSize: '1.2rem',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    marginTop: '20px',
+    boxSizing: 'border-box' as const,
+    background: '#fff',
+    color: '#343434',
+    fontFamily: 'monospace'
+  }
+}
