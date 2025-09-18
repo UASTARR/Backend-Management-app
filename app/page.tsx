a4994bc786ec6032752466a2ec16abdd515592c6
Kevin Cao <kcao36@outlook.comgit config --global user.email kcao36@outlook.comclear>
2025-09-18 12:13:24 -0600

    added auth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
-export default function Home() {
+
+import { signIn } from "@/auth"
+ 
+export default function SignIn() {
   return (
-    <div style={styles.body}>
-      <div style={styles.header}>
-        <img src="/STARRLogoWhite.png" alt="STARR Logo" style={styles.logo} />
+    <div>
+      <div style={styles.body}>
+        <div style={styles.header}>
+          <img src="/STARRLogoWhite.png" alt="STARR Logo" style={styles.logo} />
+          <div style={styles.title}>UASTARR<br/> MANAGEMENT</div>
+        </div>
+
+        {/* <div style={styles.inputBoxes}>
+          <input 
+            type="text"
+            name="name"
+            placeholder="username"
+            style={styles.inputs}/>
+          <input 
+            type="password"
+            name="password"
+            placeholder="password"
+            style={styles.inputs}/>
+
+        </div> */}
+
+        <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+        style={styles.form}
+      >
+  <button type="submit" style={styles.googleButton}>Signin with Google</button>
+      </form>
+      </div>
+    </div>
+  )
+} 
+
+// export default function Home() {
+//   return (
+//     <div style={styles.body}>
+//       <div style={styles.header}>
+        {/* <img src="/STARRLogoWhite.png" alt="STARR Logo" style={styles.logo} />
         <div style={styles.title}>UASTARR<br/> MANAGEMENT</div>
       </div>
 
@@ -22,7 +62,7 @@ export default function Home() {
 
     </div>
   );
-}
+} */}
 
 const styles = {
   body: {
@@ -55,7 +95,7 @@ const styles = {
     margin: 10
   },
   inputBoxes: {
-    margin: 300,
+    margin: 30,
     display: 'flex',
     flexDirection: 'column' as const
   },
@@ -71,5 +111,27 @@ const styles = {
     background: '#fff',
     color: '#343434',
     fontFamily: 'monospace'
-  }
+  },
+  form: {
+    marginTop: '40px',
+    display: 'flex',
+    justifyContent: 'center',
+    border: '2px solid #4285F4',
+    background: '#fff',
+    borderRadius: '8px',
+    padding: '16px',
+    boxShadow: '0 2px 8px rgba(0,0,0,0.10)',
+  },
+googleButton: {
+  padding: '12px 32px',
+  fontSize: '1.2rem',
+  borderRadius: '6px',
+  border: 'none',
+  background: '#4285F4',
+  color: '#fff',
+  fontWeight: 'bold',
+  cursor: 'pointer',
+  boxShadow: '0 2px 8px rgba(0,0,0,0.15)',
+  marginTop: '10px',
+},
 }
